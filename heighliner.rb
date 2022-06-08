27f2361f42fa2e69f90d206fe65cd4056543ef21
Hongchao Deng <hongchaodeng1@gmail.com>
2022-06-08 17:18:42 +0800

    Brew formula update for heighliner version v0.3.6

--- heighliner.rb
+++ heighliner.rb
@@ -5,20 +5,20 @@
 class Heighliner < Formula
   desc "Heighliner is an application development platform with the state-of-the-art cloud native stacks"
   homepage "https://github.com/h8r-dev/heighliner"
-  version "0.3.5"
+  version "0.3.6"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://dl.h8r.io/hln/releases/0.3.5/heighliner_v0.3.5_darwin_arm64.tar.gz"
-      sha256 "e0b4567e528cd392f19a56f79bbb54a4a2801d0c2a662cc7095cf85386cec3a6"
+    if Hardware::CPU.intel?
+      url "https://dl.h8r.io/hln/releases/0.3.6/heighliner_v0.3.6_darwin_amd64.tar.gz"
+      sha256 "0efcbfc2145c4eec9a7f9d8f2f9b887a030e7a3d9aed40b25e73c3f475131979"
 
       def install
         bin.install "hln"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://dl.h8r.io/hln/releases/0.3.5/heighliner_v0.3.5_darwin_amd64.tar.gz"
-      sha256 "5d922ae41d9fc0c83a7e5222083b7a4bc19aaea7ce279eda49bc664968f62f66"
+    if Hardware::CPU.arm?
+      url "https://dl.h8r.io/hln/releases/0.3.6/heighliner_v0.3.6_darwin_arm64.tar.gz"
+      sha256 "301687735d4d8f7e0ceb431e049366b021932a75fe908b21dd9aff0b75d187a5"
 
       def install
         bin.install "hln"
@@ -27,17 +27,17 @@ class Heighliner < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://dl.h8r.io/hln/releases/0.3.5/heighliner_v0.3.5_linux_amd64.tar.gz"
-      sha256 "5a889dabef893457036e9811173333001a87532d4c8ab3cc823040e82250b2fe"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://dl.h8r.io/hln/releases/0.3.6/heighliner_v0.3.6_linux_arm64.tar.gz"
+      sha256 "c31d239f3321a26cff718a91337b5c9e5177759841dc66985eb7658914b0ff54"
 
       def install
         bin.install "hln"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://dl.h8r.io/hln/releases/0.3.5/heighliner_v0.3.5_linux_arm64.tar.gz"
-      sha256 "78b347954f13240557e95408758bb064ef896839ec74fff9cb48665ec1581fa9"
+    if Hardware::CPU.intel?
+      url "https://dl.h8r.io/hln/releases/0.3.6/heighliner_v0.3.6_linux_amd64.tar.gz"
+      sha256 "b7a70a5af985ffdc11b4ee7213f9925ee3b80e382e46412ba29552613461f1ac"
 
       def install
         bin.install "hln"
